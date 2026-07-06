621266360bb35e852bfa0bbcb27665f25f2abdcb
bangarukondabollapally <bollapallybangarukonda@gmail.com>
2026-07-06 19:51:38 +0530

    feat: solve datatypes practice challenges

--- 03-operators/operator.js
+++ 03-operators/operator.js
@@ -53,3 +53,57 @@ console.log(typeof a);
 let arr = [];
 console.log(typeof arr);
 console.log(arr instanceof Array);
+
+
+
+
+// ------------------ Challenge 1 - String Conversion ------------------
+
+console.log("10" + 1); // 101 (string concatination)
+console.log("10" - 1); // 9 (string converted to number)
+console.log(true + false); // 1 (true -> 1, false -> 0)
+console.log(!!"Sheryians"); // true (string to boolean)
+
+
+
+// ------------------ Challenge 2 - String to Number Conversion ------------------
+
+let str = "42";
+console.log(str); // "42" (string)
+console.log(+str); // 42 (number)
+console.log(typeof +str); // number
+
+
+
+// ------------------ Challenge 3 - Ternary Operator ------------------
+
+let age = 17;
+let msg = age >= 18 ? "Adult" : "Minor";
+console.log(msg); // Minor
+
+
+
+// ------------------ Challenge 4 - Calculator with Ternary ------------------
+
+function calc(a, b, operator) {
+    return operator === '+' ? a + b :
+           operator === '-' ? a - b :
+           operator === '*' ? a * b :
+           operator === '/' ? a / b :
+           "Invalid operator";
+}
+
+console.log(calc(5, 2, '*')); // 10
+
+
+
+// ------------------ Challenge 5 - Grading System ------------------
+
+let marks = 82;
+
+let grade = marks >= 85 ? "Excellent" :
+            marks >= 70 ? "Good" :
+            marks >= 50 ? "Average" :
+            "Fail";
+
+console.log(grade); // Good

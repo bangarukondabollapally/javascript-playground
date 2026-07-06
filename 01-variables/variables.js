a71f756b8ba881da5591651346f22e047b7640fe
bangarukondabollapally <bollapallybangarukonda@gmail.com>
2026-07-06 19:01:42 +0530

    feat: add variables examples

---
+++ 01-variables/variables.js
@@ -0,0 +1,7 @@
+// ------------------ Variable Declarations ------------------
+
+var v = "function scoped";
+let l = "block scoped, reassignable";
+const c = "block scoped, not reassignable";
+
+console.log(v, l, c);

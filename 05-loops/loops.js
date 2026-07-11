ec7e409313f18f405531090b730fa29192c635d6
bangarukondabollapally <bollapallybangarukonda@gmail.com>
2026-07-11 22:08:05 +0530

    feat: solve loops practice challenges

--- 05-loops/loops.js
+++ 05-loops/loops.js
@@ -63,3 +63,66 @@ for(let i=1; i<11;i++){
     }
     console.log(i);
 }
+
+
+
+// ------------------ Challenge 1 - 1 to 10 numbers using for loop ------------------
+for(let i=1;i<11;i++){
+    console.log(i);
+}
+
+
+// ------------------ Challenge 2 - Even numbers between 1 and 20 ------------------
+for(let i=1;i<21;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+
+// ------------------ Challenge 3 - Reverse a string using a loop ------------------
+let str = "hello";
+let rev = "";
+for(let i=str.length-1;i>=0;i--){
+    rev += str[i];
+}
+console.log(rev);
+
+
+// ------------------ Challenge 4 - Sum of all numbers in an array using a loop ------------------
+let arr = [1,2,3,4,5];
+let sum = 0;
+for(let i=0;i<arr.length;i++){
+    sum += arr[i];
+}
+console.log(sum);
+
+
+// ------------------ Challenge 5 - Use continue to skip a specific number ------------------
+for(let i=1;i<11;i++){
+    if(i==5){
+        continue;
+    }
+    console.log(i);
+}
+
+
+// ------------------ Challenge 6 - Guess the number game – use while to ask until correct ------------------
+let num = 5;
+let guess = null;
+
+while (guess != num) {
+  guess = prompt("Guess the number");
+  if (guess === null) break; // user cancelled
+  console.log(guess == num ? "Correct" : "Wrong");
+}
+
+
+// ------------------ Challenge 7 - Print a triangle pattern using * ------------------
+for (let i = 1; i <= 5; i++) {
+    let line = "";
+    for (let j = 1; j <= i; j++) {
+        line += "*";
+    }
+    console.log(line);
+}
